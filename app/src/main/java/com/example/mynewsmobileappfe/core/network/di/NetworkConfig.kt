2f3db45d78e1f6e26d84ef5be0bc3de0072a4882
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-12-14 21:38:26 +0900

    🔗Chore: dev BASE_URL 설정 -> 10.0.2.2:8080

---
+++ app/src/main/java/com/example/mynewsmobileappfe/core/network/di/NetworkConfig.kt
@@ -0,0 +1,9 @@
+package com.example.mynewsmobileappfe.core.network.di
+
+/**
+ * 네트워크 공통 설정값
+ */
+object NetworkConfig {
+    // 에뮬레이터용, 실제 배포 시 환경별로 변경 필요
+    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+}
